b707ac2ae8b209fffc396d29b6c9fddba9c93ddb
Jordan Keller <keller.jc@gmail.com>
2023-04-07 11:40:40 -0700

    Add calculator functions

---
+++ main.js
@@ -0,0 +1,55 @@
+let firstNumber;
+let secondNumber;
+let operator;
+
+const add = function (firstNumber, secondNumber) {
+    let addNumbers = firstNumber + secondNumber
+    return addNumbers
+
+};
+
+const subtract = function (firstNumber, secondNumber) {
+    let subtractNumbers = firstNumber - secondNumber
+    return subtractNumbers
+
+};
+
+
+
+const multiply = function (firstNumber, secondNumber) {
+    let multNumbers = firstNumber * secondNumber
+    return multNumbers
+};
+
+const divide = function (firstNumber, secondNumber) {
+    let divNumbers = firstNumber / secondNumber
+    return divNumbers
+}
+/*
+const sum = function ([...nums]) {
+    let sumNumbers = 0
+    for (let num of nums) {
+        sumNumbers += Number(num)
+    }
+    return sumNumbers
+
+};
+
+const power = function (num, power) {
+    let powerNumber = Number(num) ** Number(power)
+    return powerNumber
+
+};
+
+const factorial = function (num) {
+    if (num === Number(0) || num === Number(1)) {
+        return Number(1)
+    } else if (num > 1) {
+        for (let i = num - 1; i >= 1; i--) {
+            num *= i
+        }
+
+        return num
+    }
+};
+*/
