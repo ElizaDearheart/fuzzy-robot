cda76e901612d5cb270da2a626897e9854f1a926
Jordan Keller <keller.jc@gmail.com>
2024-05-10 11:12:51 -0700

    Update to add event listener to keys

--- main.js
+++ main.js
@@ -1,6 +1,89 @@
+let calculator = document.querySelector('.fullcalc')
+let keys = calculator.querySelector('.calculator-keys')
 let firstNumber;
 let secondNumber;
 let operator;
+let display = document.querySelector('.calc-display');
+let clicked_keys = document.querySelectorAll('.calculator-keys')
+
+//buttons//
+
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        let key = e.target
+        let op = key.dataset.op
+        if (!op) {
+            console.log('number key')
+        }
+
+        if (
+            op === 'add' ||
+            op === 'subtract' ||
+            op === 'multiply' ||
+            op === 'divide'
+        ) {
+            console.log('op key')
+        }
+
+        if (op === 'decimal') {
+            console.log('decimal key')
+        }
+
+        if (op === 'clear') {
+            console.log('clear key')
+        }
+
+        if (op === 'equals') {
+            console.log('equals key')
+        }
+
+    }
+})
+
+
+
+
+//function displayText(e) {
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        let key = e.target;
+        let op = key.dataset.op
+        let sum = key.dataset.sum
+        let keyContent = key.textContent
+        let displayedNumber = display.textContent
+
+
+        if (!op) {
+            if (displayedNumber === '0') {
+                display.textContent = keyContent
+            } else {
+                display.textContent = displayedNumber + keyContent
+                let firstNumber = display.textContent
+                console.log(firstNumber)
+            }
+        }
+
+        if (op) {
+            if (op === 'clear') {
+                display.textContent = '0'
+            }
+
+            else {
+                display.textContent = displayedNumber + keyContent
+                let operator = keyContent
+                console.log(operator)
+            }
+
+
+
+        }
+
+
+    }
+})
+
+
+
 
 const add = function (firstNumber, secondNumber) {
     let addNumbers = firstNumber + secondNumber
