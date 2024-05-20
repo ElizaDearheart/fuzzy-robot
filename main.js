a75da18f23c49a989b15456af09093adb734c119
Jordan Keller <keller.jc@gmail.com>
2024-05-20 16:11:36 -0700

    Add backspace functionality

--- main.js
+++ main.js
@@ -6,95 +6,92 @@ let operatorChosen = ""
 let display = document.querySelector('.calc-display');
 let clicked_keys = document.querySelectorAll('.calculator-keys')
 
-function calculate() {
 
-    keys.addEventListener('click', e => {
-        let key = e.target
-        let op = key.dataset.op
-        let sum = key.dataset.sum
-        let dec = key.dataset.dec
-        let keyContent = key.textContent
-        let displayedNumber = display.textContent
 
-        if (e.target.matches('button')) {
+keys.addEventListener('click', e => {
+    let key = e.target
+    let op = key.dataset.op
+    let sum = key.dataset.sum
+    let dec = key.dataset.dec
+    let back = key.dataset.back
+    let keyContent = key.textContent
+    let displayedNumber = display.textContent
 
-            if (!op && !sum) {
+    if (e.target.matches('button')) {
 
-                if (displayedNumber === '0') {
-                    display.textContent = keyContent
-                    inputNumber = display.textContent
+        if (!op && !sum) {
 
-                    console.log(inputNumber + "input")
+            if (displayedNumber === '0') {
+                display.textContent = keyContent
+                inputNumber = display.textContent
 
-                } else {
-                    display.textContent = displayedNumber + keyContent
-                    inputNumber = display.textContent
+                console.log(inputNumber + "input")
 
-                    console.log(inputNumber + "input")
-                    console.log(inputNumber.length)
+            } else {
+                display.textContent = displayedNumber + keyContent
+                inputNumber = display.textContent
 
-                }
-            }
-            /*
-            else if (dec) {
-
-            }
-            */
-            else if (op) {
-                if (op === 'clear') {
-                    display.textContent = '0'
-                    inputNumber = ""
-                    operatorChosen = ""
-                    console.log('clear')
-                } else if (op === 'add' ||
-                    op === 'subtract' ||
-                    op === 'multiply' ||
-                    op === 'divide') {
-                    display.textContent = displayedNumber + keyContent
-                    operator = keyContent
-                    operatorChosen = operator
-                    console.log(operator + 'op')
-                }
-
-            }
-
-            else if (sum) {
-                let array = inputNumber.split(operatorChosen)
-                let firstNumber = array[0]
-                let secondNumber = array[1]
-                console.log('equals key')
-                if (operatorChosen === '+') {
-                    let answer = parseFloat(+firstNumber + +secondNumber).toPrecision(3)
-                    display.textContent = answer
-                    console.log('equals' + answer)
-
-                } else if (operatorChosen === '-') {
-                    let answer = parseFloat(+firstNumber - +secondNumber).toPrecision(3)
-                    display.textContent = answer
-                    console.log('equals' + answer)
-                } else if (operatorChosen === 'x') {
-                    let answer = parseFloat(firstNumber * secondNumber).toPrecision(3)
-                    display.textContent = answer
-                    console.log('equals' + answer)
-                } else if (operatorChosen === '/') {
-                    if (secondNumber === '0') {
-                        display.textContent = 'error'
-                    }
-                    else {
-                        let answer = parseFloat(firstNumber / secondNumber).toPrecision(3)
-                        display.textContent = answer
-                        console.log('equals' + answer)
-                    }
-                }
-
+                console.log(inputNumber + "input")
+                console.log(inputNumber.length)
 
             }
         }
-    })
-}
+
+        else if (op) {
+            if (op === 'clear') {
+                display.textContent = '0'
+                inputNumber = ""
+                operatorChosen = ""
+                console.log('clear')
+            } else if (op === 'backspace') {
+                inputNumber = inputNumber.slice(0, -1)
+                display.textContent = inputNumber
+            } else if (op === 'add' ||
+                op === 'subtract' ||
+                op === 'multiply' ||
+                op === 'divide') {
+                display.textContent = displayedNumber + keyContent
+                operator = keyContent
+                operatorChosen = operator
+                console.log(operator + 'op')
+            }
+
+        }
+
+        else if (sum) {
+            let array = inputNumber.split(operatorChosen)
+            let firstNumber = array[0]
+            let secondNumber = array[1]
+            console.log('equals key')
+            if (operatorChosen === '+') {
+                let answer = parseFloat(+firstNumber + +secondNumber).toPrecision(3)
+                display.textContent = answer
+                console.log('equals' + answer)
+
+            } else if (operatorChosen === '-') {
+                let answer = parseFloat(+firstNumber - +secondNumber).toPrecision(3)
+                display.textContent = answer
+                console.log('equals' + answer)
+            } else if (operatorChosen === 'x') {
+                let answer = parseFloat(firstNumber * secondNumber).toPrecision(3)
+                display.textContent = answer
+                console.log('equals' + answer)
+            } else if (operatorChosen === '/') {
+                if (secondNumber === '0') {
+                    display.textContent = 'error'
+                }
+                else {
+                    let answer = parseFloat(firstNumber / secondNumber).toPrecision(3)
+                    display.textContent = answer
+                    console.log('equals' + answer)
+                }
+            }
 
 
-calculate()
+        }
+    }
+})
+
 
 
 
